933513be503f347e9b4c1ae3d413c7c43dbc7855
Sanjay Sukhwani <sanjayasukhwani@gmail.com>
2024-07-09 12:06:15 +0530

    New Added : HeroSection Added with Spotlight Component of Aceternity | Added Animations and addVariablesForColors Plugin in tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 // This Page will Contain the Content that we need to show on the Web Site
 
+import HeroSection from "@/components/HeroSection";
+
 // It is similar like the React Functional Component
 export default function Home() {
     return (
         <main className="min-h-screen bg-black antialiased bg-grid-white/[0.0]">
-            <h1 className="text-4xl font-bold m-3 text-center">Music Academy - Chai Aur Code</h1>
+            {/* <h1 className="text-4xl font-bold m-3 text-center">Music Academy - Chai Aur Code</h1> */}
+            <HeroSection/>
         </main>
     );
 }
